nifgraup/butternut

commit 67a7d472833d0a7eeaf4b630501d0c3a1019f8b3

determine block emptiness at minify time, in case var is activated outside the block (ugh)

src/program/BlockStatement.js
@@ -73,7 +73,7 @@ export default class BlockStatement extends Node {
 	canSequentialise () {
 		for ( let i = 0; i < this.body.length; i += 1 ) {
 			const node = this.body[i];
-			if ( !node.skip && !node.canSequentialise() ) return false;
+			if ( !node.skip && !node.canSequentialise() ) return false; // TODO what if it's a block with a late-activated declaration...
 		}
 
 		return true;
@@ -154,14 +154,15 @@ export default class BlockStatement extends Node {
 				maybeReturnNode.skip = true;
 			}
 		}
+	}
 
+	isEmpty () {
 		for ( let i = 0; i < this.body.length; i += 1 ) {
 			const node = this.body[i];
-			if ( !node.skip ) {
-				this.skip = false;
-				break;
-			}
+			if ( !node.skip ) return false;
 		}
+
+		return true;
 	}
 
 	minify ( code ) {

src/program/Node.js
@@ -81,6 +81,10 @@ export default class Node {
 		}
 	}
 
+	isEmpty () {
+		return this.skip;
+	}
+
 	findVarDeclarations ( varsToHoist ) {
 		for ( var key of this.keys ) {
 			const value = this[ key ];

src/program/types/DoWhileStatement.js
@@ -3,7 +3,7 @@ import Node from '../Node.js';
 export default class DoWhileStatement extends Node {
 	minify ( code ) {
 		// special case
-		if ( this.body.skip ) {
+		if ( this.body.isEmpty() ) {
 			code.overwrite( this.start + 2, this.test.start, ';while(' );
 		}
 

src/program/types/IfStatement.js
@@ -128,7 +128,7 @@ export default class IfStatement extends Node {
 			this.consequent.getPrecedence() < targetPrecedence;
 
 		// special case – empty consequent
-		if ( this.consequent.skip ) {
+		if ( this.consequent.isEmpty() ) {
 			const canRemoveTest = this.test.type === 'Identifier' || this.test.getValue() !== UNKNOWN; // TODO can this ever happen?
 
 			if ( this.alternate ) {
@@ -185,7 +185,7 @@ export default class IfStatement extends Node {
 		}
 
 		// special case - empty alternate
-		if ( this.alternate && this.alternate.skip ) {
+		if ( this.alternate && this.alternate.isEmpty() ) {
 			// don't minify alternate
 			this.consequent.minify( code );
 			code.remove( this.consequent.end, this.end );

src/program/types/WhileStatement.js
@@ -15,7 +15,7 @@ export default class WhileStatement extends Node {
 		}
 
 		// special case — empty body
-		if ( this.body.skip ) {
+		if ( this.body.isEmpty() ) {
 			code.appendLeft( this.body.start, ';' );
 			code.remove( this.body.start, this.body.end );
 		}

src/program/types/shared/LoopStatement.js
@@ -23,7 +23,7 @@ export default class LoopStatement extends Node {
 		if ( this.scope ) this.scope.mangle( code );
 
 		// special case — empty body
-		if ( this.body.skip ) {
+		if ( this.body.isEmpty() ) {
 			code.appendLeft( this.body.start, ';' );
 			code.remove( this.body.start, this.body.end );
 		}

commit edc4d65f531dda0ad9fed31aa998cb0eb39e0482

fix #162 blocks are skipped
regressed in 67a7d472833d0a7eeaf4b630501d0c3a1019f8b3, reinstate
required code

src/program/BlockStatement.js
@@ -154,6 +154,14 @@ export default class BlockStatement extends Node {
 				maybeReturnNode.skip = true;
 			}
 		}
+
+		for ( let i = 0; i < this.body.length; i += 1 ) {
+			const node = this.body[i];
+			if ( !node.skip ) {
+				this.skip = false;
+				break;
+			}
+		}
 	}
 
 	// TODO remove block.isEmpty() in favour of block.skip — this is a hangover from

test/samples/dead-code-elimination.js
@@ -113,6 +113,12 @@ module.exports = [
 		description: 'removes empty block',
 		input: `{1}`,
 		output: ``
+	},
+
+	{
+		description: 'preserves used block',
+		input: `{b()}`,
+		output: `{b()}`
 	}
 
 	// TODO uncalled functions, unused variables...